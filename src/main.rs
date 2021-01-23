fbaf4b57fd36b443632b2323246ab91783d81806
quadruple-output <57874618+quadruple-output@users.noreply.github.com>
2021-01-23 23:52:02 +0100

    first coordinates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+/*
+  bevy_egui example: https://github.com/mvlabat/bevy_egui/blob/main/examples/ui.rs
+*/
+
+mod ui;
+
+use bevy::input::system::exit_on_esc_system;
+use bevy::prelude::*;
+use bevy_egui::{EguiPlugin, EguiSettings};
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_resource(ClearColor(Color::BLACK))
+        .add_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_system(exit_on_esc_system.system())
+        .add_plugin(EguiPlugin)
+        .add_system(update_ui_scale_factor.system())
+        .add_plugin(ui::Plugin)
+        .run();
+}
+
+fn update_ui_scale_factor(mut egui_settings: ResMut<EguiSettings>, windows: Res<Windows>) {
+    if let Some(window) = windows.get_primary() {
+        egui_settings.scale_factor = 1.0 / window.scale_factor();
+    }
 }
