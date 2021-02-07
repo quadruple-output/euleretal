22683b1e35e338ef426cd8de2b185232e7748e56
quadruple-output <57874618+quadruple-output@users.noreply.github.com>
2021-02-07 18:31:47 +0100

    logarithmic slider prototype

--- src/step_size.rs
+++ src/step_size.rs
@@ -1,7 +1,7 @@
 use egui::{
     color::Hsva,
     color_picker::{color_edit_button_hsva, Alpha},
-    Ui,
+    Slider, Ui,
 };
 
 pub struct StepSize {
@@ -23,10 +23,15 @@ impl StepSize {
         ui.horizontal(|mut ui| {
             color_edit_button_hsva(&mut ui, &mut self.color, Alpha::BlendOrAdditive);
             ui.text_edit_singleline(&mut self.label);
-            ui.add(egui::DragValue::f32(&mut self.dt).fixed_decimals(2));
-            if self.dt < 0.001 {
-                self.dt = 0.01;
-            }
+            // ui.add(egui::DragValue::f32(&mut self.dt).fixed_decimals(2));
+            // if self.dt < 0.001 {
+            //     self.dt = 0.01;
+            // }
+            let mut ln_dt = self.dt.ln();
+            ui.add(Slider::f32(&mut ln_dt, 0.01f32.ln()..=2.0f32.ln()));
+            self.dt = ln_dt.exp();
+            ui.label(format!("{:.4}", self.dt));
+            //
             //ui.add(Slider::f32(&mut self.dt, 0.01..=2.));
             //
             // let mut dt: String = format!("{}", self.dt);
