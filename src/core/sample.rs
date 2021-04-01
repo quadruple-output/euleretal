6b656193acdb9325fde379a03dd3ed31fdd2f179
quadruple-output <57874618+quadruple-output@users.noreply.github.com>
2021-04-01 22:29:07 +0200

    sample data structure

--- src/core/sample.rs
+++ src/core/sample.rs
@@ -1,5 +1,35 @@
 use crate::prelude::*;
 
+pub type Point = Vec3;
+pub type Acceleration = Vec3;
+pub type Velocity = Vec3;
+
+pub struct Samples {
+    steps: Vec<Step>,
+    step_points: Vec<Point>,
+    aux_points: Vec<Point>,
+    aux_points_per_step: usize,
+    aux_point_dependencies: Vec<PointDependency>,
+}
+
+struct Step {
+    time: R32,
+    duration: R32,
+    velocity: Velocity,
+    acceleration: Acceleration,
+}
+
+struct PointDependency {
+    predecessor_idx: usize,
+    successor_idx: usize,
+    weight: Fraction,
+}
+
+struct Fraction {
+    numerator: usize,
+    denominator: usize,
+}
+
 #[derive(Clone, Copy, Debug, Default)]
 pub struct Sample {
     /// Step Number
